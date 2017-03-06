30a142d6bd890922f4d8e0b11eae79ddf71213e2
yogesh <yjoon45@gmail.com>
2017-03-06 15:09:07 +0530

    Add Initials

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const { resolve } = require('path');
+
+module.exports = env => {
+  return {
+    context: resolve(__dirname, 'src'),
+    entry: './app.js',
+    devtool: env === 'development' ? 'eval' : 'sourcemap',
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'react'],
+              plugins: ['react-html-attrs', 'transform-object-rest-spread']
+            }
+          }
+        }
+      ]
+    },
+    plugins: env === 'development' ? [] : [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      new webpack.optimize.UglifyJsPlugin()
+    ],
+    output: {
+      path: resolve(__dirname, 'dist'),
+      filename: './js/app.bundle.js'
+    }
+  }
+};
